237a16b87ff7573603c9ac8b64ce9137d0dc2660
Mairaj Pirzada <thisismairaj@gmail.com>
2020-04-14 22:50:51 +0500

    Add files via upload

--- strings.py
+++ strings.py
@@ -1,61 +1,33 @@
-# DATA TYPES
+# NOTES
+
 # strings
-# integer
-# arrays(sets,tuples,lists,dictionaries)
-# object
-# resource
+# numbers
+# basics string functions
+# logical operators
+# operators
+# type casting
+# files
+# different types of arrays.. tuples, sets, lists
 
-headline = "first string"
-age = 19
-age1 = "34" #string, cuz of quotes
-megapixels = 2.3 # float or double
+# data types
+# integer, string, array, object
 
-# print(type(megapixels))
+school = "tHiS tHe kIng" # string
+age = 15 # integer
+age1 = "15"
+print(type(age))
+print(type(age1))
+# type() is function, not a method.
 
-# print(type(headline))
+# index batata hai ke string mein aik word/alphabet konse number pe araha hai
+print(school.index('I', 3))
 
-# METHOD VS FUNCTION
+print(school.capitalize()) # sentence ke pehle alphabet ko upper kardega
+print(school.upper()) # self explanatory
+print(school.lower())
 
-# headline.upper() # this a method
-# type() # this a function
-# len() # this a function
+school = "this is king i i i i i "
 
-# print(age)
-
-# print(type(headline))
-# print(type(age1))
-
-# STRING METHODS
-
-name = "kinSDsdf kd asdl "
-print(name.capitalize())
-print(name.upper())
-print(name.lower())
-print(name.swapcase())
-print(name.index("sdf"))
-
-
-# for OWAIS!!! index batata hai konse number pe string hai
-
-name1 = "guarpdian ublic pschool"
-print(name1.index('p', 6, 17))
-# name.index({kya search karna hai}, [kaha se start], [kaha end karna hai])
-# [] mean optional
-# {} requirement, owais ne pucha tha
-
-# 6 bata raha ke search kaha se shuru karni hai, cuz owais ne pucha tha
-
-sentence = 'Python proooogramming is fun.'
-
-print("is number pe 'o' hai: ", sentence.index("o"))
-print("o konsi jaghon per hai: ", sentence.find("z"))
-
-print(type(sentence))
-# do cheezon ko connect karna is concatenation, using "," comma
-
-print(sentence.count('z'))
-
-# kuch methods core ke hain, kuch methods khud banasakte hain
-
-# app.getPopularArticles() #example of a khud ka method
+print(school.count(" ", 8))
+# count, ginta hai ke kitne alphabets/words hain...
 
