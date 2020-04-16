defbe2d16413e8605cbb7319b044692e5ec0647c
Mairaj Pirzada <thisismairaj@gmail.com>
2020-04-16 22:50:53 +0500

    Add files via upload

---
+++ strings4.py
@@ -0,0 +1,69 @@
+# .replace({kya change karna hai}, {kisse change karna hai}, [kitni dafa change karna hai])
+
+string = "Python is for beyginners"
+
+if "y" not in string:
+    print("yes, y is in", string)
+exit()
+
+
+
+# print(string.replace("y", "i", 2))
+
+# .count({kya count karna hai})
+# print(string.count('i'))
+
+# if 12 >= 6
+# if this_is_true and this_too_true
+# if user_is_right
+# if loan_payment_is_due
+#
+# if function_says_true()
+# if function_says_overweight()
+# if time_function_says_its_10()
+# if that_actress_over_40(celeb_name)
+# if this_actress_age()
+# if celeb_died(celeb_name)
+
+def that_actress_over_40(celeb_name):
+    if celeb_name == "shahrukh":
+        return True
+    elif celeb_name == "tigershroff":
+        return False
+    elif celeb_name == "mairajkhusra":
+        return False
+    else:
+        return False
+
+def ye_bandi_single_hai(bandi):
+    if bandi == "darij":
+        return "khusra is avaialble for another khusra"
+        return "but homosexuality not allowed"
+    elif bandi == "maha":
+        return "wo single hai, per interested nhi hai"
+    elif bandi == "umerkishahzadi":
+        return False
+    else:
+        return False
+
+given_bandi = input("Ye bandi single hai? ")
+print(ye_bandi_single_hai(given_bandi))
+exit()
+
+# user_input = input("Ye celeb 40 saal ke uper hai ya nhi? ")
+# print(that_actress_over_40(user_input))
+# exit()
+
+if "i" in string:
+    print("was found")
+
+# xyz in string
+
+# booleans. true and false
+
+
+# abs()
+# round()
+# len()
+#
+# str.replace()
